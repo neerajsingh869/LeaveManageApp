a06d9cda03d67ccf8594da1baaf7e4caa5f3ea41
Neeraj Singh <869neerajsingh@gmail.com>
2024-03-07 15:41:30 +0530

    feat: implement ui and logic for navbar page

---
+++ src/components/nav/Navbar.js
@@ -0,0 +1,52 @@
+import { NavLink, Outlet } from "react-router-dom";
+import { useUserContextValue } from "../../contexts/userContext";
+import styles from "./Navbar.module.css";
+import companyImage from "../../assets/company.png";
+import logoutImage from "../../assets/logout.png";
+import signinImage from "../../assets/signin.png";
+
+const Navbar = () => {
+    const { isSignedIn, setIsSignedIn, userInfo } = useUserContextValue();
+
+    return (
+        <>
+            <header>
+                <nav>
+                    <NavLink className={ styles.navHome } to="/">
+                        <img src={ companyImage } alt="Genpact" />
+                        Genpact
+                    </NavLink>
+                    <ul className={ styles.navItemsContainer }>
+                        { isSignedIn ? (
+                            <>
+                                <li className={ `${ styles.navItem } ${ styles.ohrItem }` }>
+                                    Logged in as: { userInfo.ohr }
+                                </li>
+                                <li className={ styles.navItem } onClick={ () => {
+                                    setIsSignedIn(false);
+                                } }>
+                                    <NavLink className={ styles.navLink } to="/">
+                                        <img src={ logoutImage } alt="Logout" />
+                                        Logout
+                                    </NavLink>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className={ styles.navItem }>
+                                    <NavLink className={ styles.navLink } to="/signin">
+                                        <img src={ signinImage } alt="Sign In" />
+                                        SignIn
+                                    </NavLink>
+                                </li>
+                            </>
+                        ) }
+                    </ul>
+                </nav>
+            </header>
+            <Outlet />
+        </>
+    )
+};
+
+export default Navbar;
