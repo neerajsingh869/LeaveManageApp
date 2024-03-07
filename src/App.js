60367ce7217a105958c65b5048c679bb60782c6d
Neeraj Singh <869neerajsingh@gmail.com>
2024-03-07 15:59:54 +0530

    feat: commit app.js and index.css files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import './App.css';
+import { app } from "./configs/firebase";
+import CustomUserContextProvider from './contexts/userContext';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Navbar from './components/nav/Navbar';
+import Page404 from './pages/error/Page404';
+import Home from './pages/home/Home';
+import PrivateRoute from './components/secure/PrivateRoute';
+import ApplyLeave from './pages/applyLeave/ApplyLeave';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
+
+  const routes = createRoutesFromElements(
+    <Route path="/" element={ <Navbar /> } errorElement={ <Page404 /> }>
+      <Route index={ true } element={ <Home /> } />
+      <Route path="signin" element={ <Login /> } />
+      <Route path="signup" element={ <Register /> } />
+      <Route path="applyleaves" element={ 
+        <PrivateRoute>
+          <ApplyLeave />
+        </PrivateRoute>
+       } />
+    </Route>
+  );
+
+  const router = createBrowserRouter(routes);
+
   return (
-    <h1>Hello</h1>
+    <CustomUserContextProvider>
+      <RouterProvider router={ router } />
+      <Toaster position="top-right" />
+    </CustomUserContextProvider>
   );
 }
 
