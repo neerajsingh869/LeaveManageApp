ffb140fa4ebbcd7ec9437deaf5a9df1d3ae3b90c
Neeraj Singh <869neerajsingh@gmail.com>
2024-03-07 15:58:15 +0530

    feat: implement page404 component

---
+++ src/pages/error/Page404.js
@@ -0,0 +1,9 @@
+import styles from "./Page404.module.css";
+
+const Page404 = () => {
+    return (
+        <h1 className={ styles.page404Header }>Page not found</h1>
+    )
+};
+
+export default Page404;
